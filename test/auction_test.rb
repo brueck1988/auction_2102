2b97294767a332671bab5c9b75e56c88cd6d8df9
brueck1988 <brueck1988@gmail.com>
2021-03-02 13:08:03 -0700

    Initial commit

---
+++ test/auction_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require require './lib/item'
+require './lib/auction'
+require './lib/attendee'
+
+class AuctionTest < Minitest::Test
+  def test_it_exists
+    assert_instance_of
+  end
+
+  def test_it_has_attributes
+    assert_equal
+  end
+end
