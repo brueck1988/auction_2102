b2ead7291d46dde44d75da546cb174b2740735cd
brueck1988 <brueck1988@gmail.com>
2021-03-02 13:12:39 -0700

    Created item test and class

--- test/item_test.rb
+++ test/item_test.rb
@@ -1,15 +1,17 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require require './lib/item'
+require './lib/item'
 require './lib/auction'
 require './lib/attendee'
 
 class ItemTest < Minitest::Test
   def test_it_exists
-    assert_instance_of
+    item1 = Item.new('Chalkware Piggy Bank')
+    assert_instance_of Item, item1
   end
 
   def test_it_has_attributes
-    assert_equal
+    item1 = Item.new('Chalkware Piggy Bank')
+    assert_equal "Chalkware Piggy Bank", item1.name
   end
 end
