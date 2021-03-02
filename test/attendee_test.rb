4b71a2d7e3f0124377a3ccfb7fb6f0fcd14103ea
brueck1988 <brueck1988@gmail.com>
2021-03-02 13:18:44 -0700

    Created attendee test

--- test/attendee_test.rb
+++ test/attendee_test.rb
@@ -1,15 +1,24 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require require './lib/item'
+require './lib/item'
 require './lib/auction'
 require './lib/attendee'
 
 class AttendeeTest < Minitest::Test
   def test_it_exists
-    assert_instance_of
+    attendee = Attendee.new(name: 'Megan', budget: '$50')
+    assert_instance_of Attendee, attendee
   end
 
   def test_it_has_attributes
-    assert_equal
+    attendee = Attendee.new(name: 'Megan', budget: '$50')
+    assert_equal "Megan", attendee.name
+    assert_equal 50, attendee.budget
   end
+
+  # def test_it_has_attributes
+  #   item1 = Item.new('Chalkware Piggy Bank')
+  #   item2 = Item.new('Bamboo Picture Frame')
+  #   assert_equal
+  # end
 end
