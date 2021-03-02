c98ceeab85bee8bdb1d074462d29af6118679463
brueck1988 <brueck1988@gmail.com>
2021-03-02 13:31:57 -0700

    Created auction test and class

--- test/auction_test.rb
+++ test/auction_test.rb
@@ -1,15 +1,17 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require require './lib/item'
+require './lib/item'
 require './lib/auction'
 require './lib/attendee'
 
 class AuctionTest < Minitest::Test
   def test_it_exists
-    assert_instance_of
+    auction = Auction.new
+    assert_instance_of Auction, auction
   end
 
   def test_it_has_attributes
-    assert_equal
+    auction = Auction.new
+    assert_equal [], auction.items
   end
 end
