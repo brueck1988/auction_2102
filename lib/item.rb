8ee5b67f9c439f19250fc5c4512bc9d63d6f8cf2
brueck1988 <brueck1988@gmail.com>
2021-03-02 14:31:05 -0700

    Added current high bid method to item class

--- lib/item.rb
+++ lib/item.rb
@@ -10,4 +10,12 @@ class Item
   def add_bid(attendee, bid)
     @bids[attendee] = bid
   end
+
+  def current_high_bid
+    @bids.max_by{|key, value| value}[1]
+    # @bids.each do |bid|
+    #   require "pry";binding.pry
+    #   bid[1].max
+    # end
+  end
 end
