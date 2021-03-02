5ab387ece7fcbcc0ad6536e4152c5fc9ac4468fd
brueck1988 <brueck1988@gmail.com>
2021-03-02 15:03:57 -0700

    Added potential revenue method and completed iteration 2

--- lib/auction.rb
+++ lib/auction.rb
@@ -1,6 +1,5 @@
 class Auction
-  attr_reader :items,
-              :bids
+  attr_reader :items
 
   def initialize
     @items = []
@@ -8,5 +7,21 @@ class Auction
 
   def add_item(item)
     @items << item
+  end
+
+  def unpopular_items
+    unpopular_items = []
+    @items.each do |item|
+      unpopular_items << item if item.bids == {}
+    end
+    unpopular_items
+  end
+
+  def potential_revenue
+    potential_revenue = 0
+    @items.each do |item|
+      potential_revenue += item.current_high_bid if item.bids != {}
+    end
+    potential_revenue
   end
 end
